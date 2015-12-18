8226df70edded572bec8e8250da13da454b979b9
Kelli Margaret <kelli.sommerdyke@gmail.com>
2015-12-18 08:30:38 -0800

    Load all Book instances

--- src/model/Book.js
+++ src/model/Book.js
@@ -3,3 +3,23 @@ function Book ( slots) {
   this.title = slots.title;
   this.year = slots.year;
 };
+
+Book.loadAll = function() {
+  var i=0, key="", keys=[], bookTableString="", bookTable={};
+  try {
+    if (localStorage["bookTable"]) {
+      bookTableString = localStorage["bookTable"];
+    }
+  } catch (e) {
+    alert ("Error when reading from Local Storage\n" + e);
+  }
+  if (bookTableString) {
+    bookTable = JSON.parse ( bookTableString);
+    keys = Object.keys( bookTable);
+    console.log ( keys.length +" books loaded.");
+    for (i=0, i < keys.length; i++) {
+      key = keys[i];
+      Book.instance[key] = Book.convertRow20bj ( bookTable[key]);
+    }
+  }
+};
